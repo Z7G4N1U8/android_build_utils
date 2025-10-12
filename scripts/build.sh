4757191c697c2fc60e1c4bd742dc71e63ac30fe0
Peace <121939750+Z7G4N1U8@users.noreply.github.com>
2025-10-12 13:42:14 +0530

    android: dump and & extract rom at same time

--- scripts/build.sh
+++ scripts/build.sh
@@ -74,9 +74,14 @@ else
     curl -fLSs "${remote_script}" | bash || handle_error "Remote sync script download or execution failed"
 fi
 
+# Dump ROM & Extract Vendor
 cd device/motorola/eqe
-./extract-files.py ../eqe-dump
-cd -
+git clone https://github.com/DumprX/DumprX.git && cd DumprX
+bash setup.sh
+bash dumper.sh https://mirrors.lolinet.com/firmware/lenomola/2024/eqe/official/RETAIL/EQE_RETAIL_15_V1UMS35H.10-67-7-2_subsidy-DEFAULT_regulatory-DEFAULT_cid50_CFC.xml.zip
+cd ..
+./extract-files.py DumprX/out
+cd ../../..
 
 # Apply patches
 patches=(
