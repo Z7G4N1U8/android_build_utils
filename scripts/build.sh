bb8c867021e36dbc729520c7c5223a6626b84344
peace <121939750+Z7G4N1U8@users.noreply.github.com>
2025-10-20 17:59:47 +0530

    refactor

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 
-# Check if at least 3 argument is provided
-if [ "$#" -lt 3 ]; then
-    echo "Error: At least 3 arguments are required."
-    exit 1
-fi
-android="${1}"
-device="${2}"
-build_type="${3}"
-
-# my repo containing patches and scripts
-build_utils="https://raw.githubusercontent.com/Z7G4N1U8/android_build_utils/refs/heads/main"
-
-# Function for centralized error handling
 handle_error() {
-    local error_message="$1"
-    echo "Error: ${error_message}."
+    echo "Error: $1."
     exit 1
 }
 
@@ -24,53 +10,33 @@ for path in $(xmllint --xpath '//project/@path' ".repo/local_manifests/default.x
     echo "Removing directory: $path"
     rm -rf "$path"
 done
-rm -rf vendor/private .repo/local_manifests
+rm -rf vendor/private .repo/local_manifests ${removals}
 
 set -o pipefail
 trap 'handle_error "An unexpected error occurred"' ERR
 
-# crave resync script
-local_script="/opt/crave/resync.sh"
-remote_script="${build_utils}/scripts/resync.sh"
-
-# Initialize ROM and Device source
 case "${android}" in
     "LineageOS")
-        repo_url="https://github.com/LineageOS/android.git"
-        repo_branch="lineage-23.0"
+        manifest="https://github.com/LineageOS/android.git"
+        branch="lineage-23.0"
+        target="bacon"
         ;;
     "EvolutionX")
-        repo_url="https://github.com/Evolution-X/manifest.git"
-        repo_branch="bka"
-        ;;
-    "RisingOS")
-        repo_url="https://github.com/RisingOS-Revived/android.git"
-        repo_branch="qpr2"
-        ;;
-    "Matrixx")
-        repo_url="https://github.com/ProjectMatrixx/android.git"
-        repo_branch="15.0"
-        ;;
-    "PixelOS")
-        repo_url="https://github.com/PixelOS-AOSP/manifest.git"
-        repo_branch="fifteen"
-        ;;
-    *)
-        handle_error "Invalid option: ${android}. Use lineage, evolution, or rising"
+        manifest="https://github.com/Evolution-X/manifest.git"
+        branch="bka"
+        target="evolution"
         ;;
 esac
-repo init --depth 1 --git-lfs --manifest-url ${repo_url} --manifest-branch ${repo_branch} || handle_error "Repo init failed"
-curl -fLSs --create-dirs "${build_utils}/manifests/${device}.xml" -o .repo/local_manifests/default.xml || handle_error "Local manifest init failed"
-git clone https://${GH_TOKEN}@github.com/Z7G4N1U8/android_vendor_private_keys vendor/private/keys || handle_error "cloning keys failed"
+repo init --depth 1 --git-lfs -u ${manifest} -b ${branch} || handle_error "Repo init failed"
+curl -fLSs --create-dirs "${utils}/manifests/${device}.xml" -o .repo/local_manifests/default.xml || handle_error "Local manifest init failed"
+git clone https://${token}@github.com/Z7G4N1U8/android_vendor_private_keys vendor/private/keys || handle_error "cloning keys failed"
 
-# check if local sync script exists. if not, use remote sync script
-if [ -f "${local_script}" ]; then
-    echo "Attempting to run local sync script: ${local_script}"
-    "${local_script}" || handle_error "Local sync script execution failed"
+if [ -f "/opt/crave/resync.sh" ]; then
+    echo "Attempting to run sync script..."
+    /opt/crave/resync.sh || handle_error "sync script execution failed"
 else
-    echo "Local sync script (${local_script}) not found."
-    echo "Attempting to download and run remote sync script from: ${remote_script}"
-    curl -fLSs "${remote_script}" | bash || handle_error "Remote sync script download or execution failed"
+    echo "Attempting to run repo sync command..."
+    repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all) || handle_error "repo sync execution failed"
 fi
 
 # Dump ROM & Extract Vendor
@@ -85,21 +51,7 @@ cd ..
 rm -rf DumprX
 cd ../../..
 
-# Apply patches
-patches=(
-    "packages/services/Telephony:2by2-Project/packages_services_Telephony/commit/6d1276ad67ec5a023e4d65cec1e0c659cf756cef"
-)
-
-for patch in "${patches[@]}"; do
-    IFS=":" read -r patch_path patch_url <<< "${patch}"
-    rm -rf ${patch_path}
-    repo sync ${patch_path}
-    cd ${patch_path}
-    curl -fLSs https://github.com/${patch_url}.patch | git am
-    cd -
-done
-
 echo "Starting build process..."
 source build/envsetup.sh
-breakfast ${device} ${build_type}
-cmka evolution
+breakfast ${device} ${build}
+cmka ${target}
